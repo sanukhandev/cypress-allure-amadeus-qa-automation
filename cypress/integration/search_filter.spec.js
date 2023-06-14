fc56a3c17405778cf02a540e86d59bd6ab19a496
Sanu Khan-Business Partner <sanu.khan-external@alfuttaim.com>
2023-06-14 12:23:56 +0400

    Optimzed the test cases

--- cypress/integration/search_filter.spec.js
+++ cypress/integration/search_filter.spec.js
@@ -1,8 +1,28 @@
-const {jsonToQueryString} = require("./utils");
-
-
-
+const {jsonToQueryString, defalutOWpayloadQuery} = require("./utils");
 describe('Flight Page', () => {
+    const waitForLowfareRequest = () => {
+        cy.wait('@lowfareRequest', {timeout: 120000});
+        cy.wait(2000);
+    };
+
+    const verifyCards = (selector, text) => {
+        cy.get(selector).each(($element) => {
+            cy.wrap($element).should('contain', text);
+        });
+    };
+    const verifyFlightCards = (selector, text) => {
+        cy.get(selector)
+            .each(($div) => {
+                cy.wrap($div).invoke('text').should('match', text);
+            });
+    };
+    const verifyFlightCount = (query) => {
+        cy.intercept('POST', '**/api/v2/offer/lowfare').as('lowfareRequest');
+        const URL = `Flight/search?${jsonToQueryString(defalutOWpayloadQuery(...query))}`;
+        cy.visit(URL);
+        cy.wait(2000);
+        waitForLowfareRequest();
+    };
     it('Verify Advanced search options', () => {
         // Go to the '/flight' page
         cy.visit('/flight');
@@ -28,200 +48,28 @@ describe('Flight Page', () => {
                 expect($labels.eq(2).text().trim()).to.equal('Refundable');
             });
     });
-    it('Verify cards and direct flights count', () => {
-        cy.intercept('POST', '**/api/v2/offer/lowfare').as('lowfareRequest');
-        const query = jsonToQueryString( {
-            "dep1": "DXB",
-            "ret1": "LON",
-            "dtt1": "22-Jun-2023",
-            "cl1": "Y",
-            "triptype": 1,
-            "adult": 1,
-            "child": 0,
-            "infant": 0,
-            "direct": true,
-            "baggage": false,
-            "key": "OW",
-            "airlines": "",
-            "ref": false,
-            "langcode": "EN",
-            "curr": "AED",
-            "ipc": false,
-            "dep2": "",
-            "ret2": "",
-            "dtt2": "",
-            "cl2": "Y",
-            "dep3": "",
-            "ret3": "",
-            "dtt3": "",
-            "cl3": ""
-        })
-        const URL = `Flight/search?${query}`
-        cy.visit(URL);
-        cy.wait(2000)
-        cy.wait('@lowfareRequest', { timeout:120000 });
-        cy.wait(2000)
-        cy.get('span.empireFlight_stopvia')
-            .each(($span) => {
-                cy.wrap($span).should('contain', 'Direct');
-            });
+    it('Verify direct flights count', () => {
+        verifyFlightCount(['direct', true]);
+        verifyCards('span.empireFlight_stopvia', 'Direct');
+    });
 
-    })
-    it('Verify cards and Refundable  flights count', () => {
-        cy.intercept('POST', '**/api/v2/offer/lowfare').as('lowfareRequest');
-        const query = jsonToQueryString( {
-            "dep1": "DXB",
-            "ret1": "LON",
-            "dtt1": "22-Jun-2023",
-            "cl1": "Y",
-            "triptype": 1,
-            "adult": 1,
-            "child": 0,
-            "infant": 0,
-            "direct": false,
-            "baggage": false,
-            "key": "OW",
-            "airlines": "",
-            "ref": true,
-            "langcode": "EN",
-            "curr": "AED",
-            "ipc": false,
-            "dep2": "",
-            "ret2": "",
-            "dtt2": "",
-            "cl2": "Y",
-            "dep3": "",
-            "ret3": "",
-            "dtt3": "",
-            "cl3": ""
-        })
-        const URL = `Flight/search?${query}`
-        cy.visit(URL);
-        cy.wait(2000)
-        cy.wait('@lowfareRequest', { timeout:120000 });
-        cy.wait(2000)
-        cy.get('div.empireFlight_refund-text')
-            .each(($span) => {
-                cy.wrap($span).should('contain', 'Refundable');
-            });
+    it('Verify refundable flights count', () => {
+        verifyFlightCount(['ref', true]);
+        verifyCards('div.empireFlight_refund-text', 'Refundable');
+    });
 
-    })
-    it('Verify cards and Emirates Flights  flights count', () => {
-        cy.intercept('POST', '**/api/v2/offer/lowfare').as('lowfareRequest');
-        const query = jsonToQueryString( {
-            "dep1": "DXB",
-            "ret1": "LON",
-            "dtt1": "22-Jun-2023",
-            "cl1": "Y",
-            "triptype": 1,
-            "adult": 1,
-            "child": 0,
-            "infant": 0,
-            "direct": false,
-            "baggage": false,
-            "key": "OW",
-            "airlines": "EK",
-            "ref": false,
-            "langcode": "EN",
-            "curr": "AED",
-            "ipc": false,
-            "dep2": "",
-            "ret2": "",
-            "dtt2": "",
-            "cl2": "Y",
-            "dep3": "",
-            "ret3": "",
-            "dtt3": "",
-            "cl3": ""
-        })
-        const URL = `Flight/search?${query}`
-        cy.visit(URL);
-        cy.wait(2000)
-        cy.wait('@lowfareRequest', { timeout:120000 });
-        cy.wait(2000)
-        cy.get('div.empireFlight_FlightNames')
-            .each(($div) => {
-                cy.wrap($div).invoke('text').should('match', /^Emirates\s/);
-            });
+    it('Verify Emirates flights count', () => {
+        verifyFlightCount(['airlines', 'EK']);
+        verifyFlightCards('div.empireFlight_FlightNames', /^Emirates\s/);
+    });
 
-    })
-    it('Verify cards and Saudi Arabian Airlines  Flights  flights count', () => {
-        cy.intercept('POST', '**/api/v2/offer/lowfare').as('lowfareRequest');
-        const query = jsonToQueryString( {
-            "dep1": "DXB",
-            "ret1": "LON",
-            "dtt1": "22-Jun-2023",
-            "cl1": "Y",
-            "triptype": 1,
-            "adult": 1,
-            "child": 0,
-            "infant": 0,
-            "direct": false,
-            "baggage": false,
-            "key": "OW",
-            "airlines": "SV",
-            "ref": false,
-            "langcode": "EN",
-            "curr": "AED",
-            "ipc": false,
-            "dep2": "",
-            "ret2": "",
-            "dtt2": "",
-            "cl2": "Y",
-            "dep3": "",
-            "ret3": "",
-            "dtt3": "",
-            "cl3": ""
-        })
-        const URL = `Flight/search?${query}`
-        cy.visit(URL);
-        cy.wait(2000)
-        cy.wait('@lowfareRequest', { timeout:120000 });
-        cy.wait(2000)
-        cy.get('div.empireFlight_FlightNames')
-            .each(($div) => {
-                cy.wrap($div).invoke('text').should('match', /^Saudi Arabian Airlines\s/);
-            });
+    it('Verify Saudi Arabian Airlines flights count', () => {
+        verifyFlightCount(['airlines', 'SV']);
+        verifyFlightCards('div.empireFlight_FlightNames', /^Saudi Arabian Airlines\s/);
+    });
 
-    })
-    it('Verify cards and Saudi Arabian Airlines And Emirates  Flights  flights count', () => {
-        cy.intercept('POST', '**/api/v2/offer/lowfare').as('lowfareRequest');
-        const query = jsonToQueryString( {
-            "dep1": "DXB",
-            "ret1": "LON",
-            "dtt1": "22-Jun-2023",
-            "cl1": "Y",
-            "triptype": 1,
-            "adult": 1,
-            "child": 0,
-            "infant": 0,
-            "direct": false,
-            "baggage": false,
-            "key": "OW",
-            "airlines": "SV,EK",
-            "ref": false,
-            "langcode": "EN",
-            "curr": "AED",
-            "ipc": false,
-            "dep2": "",
-            "ret2": "",
-            "dtt2": "",
-            "cl2": "Y",
-            "dep3": "",
-            "ret3": "",
-            "dtt3": "",
-            "cl3": ""
-        })
-        const URL = `Flight/search?${query}`
-        cy.visit(URL);
-        cy.wait(2000)
-        cy.wait('@lowfareRequest', { timeout:120000 });
-        cy.wait(2000)
-        cy.get('div.empireFlight_FlightNames')
-            .each(($div) => {
-                cy.wrap($div).invoke('text')
-                    .should('match', /^(Saudi Arabian Airlines|Emirates)\s/);
-            });
-
-    })
+    it('Verify Saudi Arabian Airlines and Emirates flights count', () => {
+        verifyFlightCount(['airlines', 'SV,EK']);
+        verifyFlightCards('div.empireFlight_FlightNames', /^(Saudi Arabian Airlines|Emirates)\s/);
+    });
 });
--- cypress/integration/utils.js
+++ cypress/integration/utils.js
@@ -6,6 +6,52 @@ const jsonToQueryString = (json) => {
 }
 
 
+const getNewDate = (days=20) => {
+    const currentDate = new Date();
+    const futureDate = new Date(currentDate.getTime() + days * 24 * 60 * 60 * 1000);
+
+    const options = { day: "2-digit", month: "short", year: "numeric" };
+    return futureDate.toLocaleDateString("en-US", options)
+        .replace(/(\w+) (\d+), (\d+)/, (match, month, day, year) =>
+            `${parseInt(day)}-${month}-${year}`
+        );
+};
+
+const defalutOWpayloadQuery = (key = '', value = null) => {
+    const payload = {
+        dep1: "DXB",
+        ret1: "LON",
+        dtt1: getNewDate(),
+        cl1: "Y",
+        triptype: 1,
+        adult: 1,
+        child: 0,
+        infant: 0,
+        direct: false,
+        baggage: false,
+        key: "OW",
+        airlines: "",
+        ref: false,
+        langcode: "EN",
+        curr: "AED",
+        ipc: false,
+        dep2: "",
+        ret2: "",
+        dtt2: "",
+        cl2: "Y",
+        dep3: "",
+        ret3: "",
+        dtt3: "",
+        cl3: ""
+    }
+    if (key && value) {
+        payload[key] = value;
+    }
+    return payload;
+}
+
 module.exports = {
-    jsonToQueryString
+    jsonToQueryString,
+    getNewDate,
+    defalutOWpayloadQuery
 }
