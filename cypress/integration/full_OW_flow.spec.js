e24a32bc5e7b1bb2e6faf5b910b88799bd867469
Sanu Khan-Business Partner <sanu.khan-external@alfuttaim.com>
2023-09-20 19:05:41 +0400

    One Way Full flow

--- cypress/integration/full_OW_flow.spec.js
+++ cypress/integration/full_OW_flow.spec.js
@@ -73,16 +73,19 @@ describe('Check the full OW flow with all filter checks', () => {
     };
 
     const getFlightDetails = () => {
-        cy.intercept('*', () => {
-        }).as('networkRequests');
+        // cy.intercept('*', () => {
+        // }).as('networkRequests');
         cy.wait(2000);
-        cy.get('.empireFlight_ListingBodycontainer').contains('Select').click({force: true})
         interceptBrandedFares();
-        cy.wait('@brandedFares', {timeout: 30000}).then((interception) => {
-            const {body} = interception.response;
-            appliedCommercialRuleIDs = getCommercialRuleIDs(body);
-            cy.allure().logStep(`Applied commercial rule IDs are ${appliedCommercialRuleIDs.join(', ')}`);
-        });
+
+        cy.get('.empireFlight_ListingBodycontainer').contains('Select').click({force: true}).then(() => {
+            cy.wait('@brandedFares', {timeout: 30000}).then((interception) => {
+                const {body} = interception.response;
+                appliedCommercialRuleIDs = getCommercialRuleIDs(body);
+                cy.allure().logStep(`Applied commercial rule IDs are ${appliedCommercialRuleIDs.join(', ')}`);
+            });
+        })
+
 
     };
 
@@ -154,119 +157,136 @@ describe('Check the full OW flow with all filter checks', () => {
         console.log(ids, 'ids' )
         return ids;
     }
+    const fillNgSelect = (controlName, value) => {
+        cy.get(`ng-select[formcontrolname="${controlName}"]`).click()
+            .find('div.ng-dropdown-panel-items')
+            .find('div.ng-option')
+            .contains(value).click();
+    }
 
     const bookFlight = () => {
-        cy.get('.flightDetailText').contains('Book Now').click({force: true})
+        const { Adults } = customerData;
+        const { title, first, last, documentNumber, issuingCountry, nationality, email, countryCode, phone } = Adults[0];
 
-        const {Adults} = customerData;
-        cy.wait('@networkRequests', {timeout: 30000});
+        cy.get('.flightDetailText').contains('Book Now').click({ force: true });
+        cy.wait('@networkRequests', { timeout: 30000 });
 
-        cy.wait(2000);
-        cy.get('div.empireF_TravelerFormBody').find('ng-select[formcontrolname="Title"]').click()
-        cy.get('div.ng-dropdown-panel-items').find('div.ng-option').contains(Adults[0].title).click()
+        cy.get('div.empireF_TravelerFormBody')
+            .find('ng-select[formcontrolname="Title"]').click()
+            .find('div.ng-option').contains(title).click();
 
-        cy.get('input[formcontrolname="FirstName"]').type(Adults[0].first)
-        cy.get('input[formcontrolname="LastName"]').type(Adults[0].last)
+        cy.get('input[formcontrolname="FirstName"]').type(first);
+        cy.get('input[formcontrolname="LastName"]').type(last);
+
         setDate('empireF_travelerDateofBirth', 'BirthDate', 'BirthMonth', 'BirthYear', '24', 'September', '1993');
-
-        cy.get('ng-select[formcontrolname="DocumentType"]').click().find('div.ng-dropdown-panel-items').find('div.ng-option').contains(Adults[0].documentType).click()
-        cy.get('input[formcontrolname="DocumentNumber"]').type(Adults[0].documentNumber)
-        cy.get('ng-select[formcontrolname="DocumentIssuingCountry"]').click().find('div.ng-dropdown-panel-items').find('div.ng-option').contains(Adults[0].issuingCountry).click()
-        cy.get('ng-select[formcontrolname="Nationality"]').click().find('div.ng-dropdown-panel-items').find('div.ng-option').contains(Adults[0].nationality).click()
-        setDate('empireF_pasPortInfo', 'DocumentIssueDay', 'DocumentIssueMonth', 'DocumentIssueYear', '11', 'January', '2018');
+        cy.get('input[formcontrolname="DocumentNumber"]').type(documentNumber)
+        fillNgSelect('DocumentIssuingCountry', issuingCountry);
+        fillNgSelect('Nationality', nationality);
         setDate('empireF_pasPortInfo', 'DocumentExpiryDay', 'DocumentExpiryMonth', 'DocumentExpiryYear', '10', 'January', '2028');
-        cy.get('input[formcontrolname="EmailAddress"]').type(Adults[0].email)
-        cy.get('ng-select[formcontrolname="phne_code"]').click().find('div.ng-dropdown-panel-items').find('div.ng-option').contains(Adults[0].countryCode).click()
-        cy.get('input[formcontrolname="MobileNo"]').type(Adults[0].phone)
-        cy.get('button').find('div').contains('Continue to payment').click()
-        cy.wait('@networkRequests', {timeout: 30000});
-        cy.wait(2000);
-        cy.get('div').contains('Proceed to Pay').click()
-        cy.wait(30000)
-        cy.wait('@networkRequests', {timeout: 30000});
-        cy.origin('https://sbcheckout.payfort.com/', () => {
-            cy.get('input#cardNoInput').type('4005550000000001')
-            cy.get('input#chNameInput').type('Test')
-            cy.get('input#expDateInput').type('05/25')
-            cy.get('input#cvvInput').type('123')
-            cy.get('button').contains('Pay').click()
-        })
-        cy.wait(30000)
-        cy.wait(30000)
-        let confirmiedComeercialRuleIDs = [];
-        interceptBookingConfirmation()
-        cy.wait('@bookingConfirmation', {timeout: 30000}).then((interception) => {
-            const {body} = interception.response;
-            confirmiedComeercialRuleIDs = getConfirmCommercialRuleIDs(JSON.parse(body.respObj));
-            cy.allure().logStep(`confirmiedComeercialRuleIDs commercial rule IDs are ${confirmiedComeercialRuleIDs.join(', ')}`);
-            console.log(confirmiedComeercialRuleIDs, 'confirmiedComeercialRuleIDs')
+
+        cy.get('input[formcontrolname="EmailAddress"]').type(email);
+        fillNgSelect('phne_code', countryCode);
+        cy.get('input[formcontrolname="MobileNo"]').type(phone);
+
+        cy.get('button').find('div').contains('Continue to payment').click();
+        cy.wait('@networkRequests', { timeout: 30000 });
+
+        proceedToPayment();
+
+        confirmBooking();
+    }
+
+    const proceedToPayment = () => {
+        cy.get('span').contains('PayFort Gateway ').click();
+        cy.get('div').contains('Proceed to Pay').click();
+
+        cy.wait('@networkRequests', { timeout: 30000 }).then((interception) => {
+
+            cy.origin('https://sbcheckout.payfort.com/', () => {
+                cy.get('input#cardNoInput').type('4005550000000001');
+                cy.get('input#chNameInput').type('Test');
+                cy.get('input#expDateInput').type('05/25');
+                cy.get('input#cvvInput').type('123');
+                cy.get('button').contains('Pay').click();
+            });
         });
-        cy.get('div').contains('Booking Summary').should('be.visible')
+    }
+
+
+    const confirmBooking = () => {
+        let confirmedCommercialRuleIDs = [];
+        interceptBookingConfirmation();
+        cy.wait('@bookingConfirmation', { timeout: 30000 }).then((interception) => {
+            const { body } = interception.response;
+            confirmedCommercialRuleIDs = getConfirmCommercialRuleIDs(JSON.parse(body.respObj));
+            logDetails(confirmedCommercialRuleIDs, 'Confirmed commercial rule IDs are');
+        });
+        checkBookingStatus();
+        checkTripDetails();
+    }
+
+    const logDetails = (details, message) => {
+        cy.allure().logStep(`${message} ${details.join(', ')}`);
+        console.log(details, message);
+    }
+
+    const checkBookingStatus = () => {
+        cy.get('div').contains('Booking Summary').should('be.visible');
         cy.get('h3.empireFlight_confirmBookingStatus')
             .invoke('text')
-            .then((text) => {
-                // Do something with the text
-                cy.allure().logStep(`Booking status is ${text}`)
-            });
+            .then((text) => cy.allure().logStep(`Booking status is ${text}`));
+        // Add other assertions or logs if needed
+    }
 
-        cy.allure().logStep(`Applied commercial rule IDs are ${appliedCommercialRuleIDs.join(', ')}`);
-        cy.allure().logStep(`Confirmed commercial rule IDs are ${confirmiedComeercialRuleIDs.join(', ')}`);
-        cy.allure().startStep('Verify applied commercial rule IDs are same as confirmed commercial rule IDs')
-        // expect(appliedCommercialRuleIDs).to.deep.equal(confirmiedComeercialRuleIDs);
-        cy.allure().endStep('passed');
-
+    const checkTripDetails = () => {
         cy.get('div.empireFlight_confirmPaytxt > span > span')
             .invoke('text')
-            .then((tripId) => {
-                // Do something with the tripId
-                cy.allure().logStep(`Trip ID is ${tripId}`)
-            });
+            .then((tripId) => cy.allure().logStep(`Trip ID is ${tripId}`));
+
         cy.get('h4.empireFlight_confirmPnr')
             .invoke('text')
-            .then((pnr) => {
-                // Do something with the pnr
-                cy.allure().logStep(`PNR is ${pnr}`)
-            });
-
-
+            .then((pnr) => cy.allure().logStep(`PNR is ${pnr}`));
+        // Add other assertions or logs if needed
     }
+
+
 
     it('should one way booking with filter checks', () => {
         cy.allure().startStep('Search for one way flight EK ')
         getFlights(['airlines', 'EK']);
-        cy.allure().endStep('passed');
-        cy.allure().startStep('Verify flight cards has Emirates')
-        verifyFlightCards('div.empireFlight_FlightNames', /^Emirates\s/);
-        cy.allure().endStep('passed');
-        cy.allure().startStep('Verify flight cards has Direct')
-        verifyCards('span.empireFlight_stopvia', 'Direct');
-        cy.allure().endStep('passed');
-
-        const filtersToApply = [
-            {filter: ' Price ', min: 100, max: 200},
-            {filter: ' Departure Timing ', min: 100, max: 200},
-            {filter: ' Departure Trip Duration ', min: 100, max: 200},
-            {filter: ' Arrival Timing ', min: 100, max: 200},
-        ];
-
-        filtersToApply.forEach(({filter, min, max}) => {
-            cy.allure().startStep(`Apply filter ${filter} with min ${min} and max ${max}`)
-            clickMorefilters();
-            adjustSlider(filter, min, 'min');
-            adjustSlider(filter, max, 'max');
-            clickFilterApply();
-            cy.allure().endStep('passed');
-        });
-
-        // Reset all filters
-        cy.allure().startStep('Reset all filters')
-        clickMorefilters();
-
-        filtersToApply.forEach(({filter}) => {
-            resetSlider(filter);
-        });
-        cy.allure().endStep('passed');
-        clickFilterApply();
+        // cy.allure().endStep('passed');
+        // cy.allure().startStep('Verify flight cards has Emirates')
+        // verifyFlightCards('div.empireFlight_FlightNames', /^Emirates\s/);
+        // cy.allure().endStep('passed');
+        // cy.allure().startStep('Verify flight cards has Direct')
+        // verifyCards('span.empireFlight_stopvia', 'Direct');
+        // cy.allure().endStep('passed');
+        //
+        // const filtersToApply = [
+        //     {filter: ' Price ', min: 100, max: 200},
+        //     {filter: ' Departure Timing ', min: 100, max: 200},
+        //     {filter: ' Departure Trip Duration ', min: 100, max: 200},
+        //     {filter: ' Arrival Timing ', min: 100, max: 200},
+        // ];
+        //
+        // filtersToApply.forEach(({filter, min, max}) => {
+        //     cy.allure().startStep(`Apply filter ${filter} with min ${min} and max ${max}`)
+        //     clickMorefilters();
+        //     adjustSlider(filter, min, 'min');
+        //     adjustSlider(filter, max, 'max');
+        //     clickFilterApply();
+        //     cy.allure().endStep('passed');
+        // });
+        //
+        // // Reset all filters
+        // cy.allure().startStep('Reset all filters')
+        // clickMorefilters();
+        //
+        // filtersToApply.forEach(({filter}) => {
+        //     resetSlider(filter);
+        // });
+        // cy.allure().endStep('passed');
+        // clickFilterApply();
         cy.allure().startStep('Verify Able to select flight for booking')
         getFlightDetails();
         cy.allure().endStep('passed');
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,13 @@
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = (on, config) => {
+    on('before:browser:launch', (browser = {}, args) => {
+        if (browser.family === 'chrome') {
+            args.push('--disable-dev-shm-usage');
+        }
+
+        return args;
+    });
     allureWriter(on, config);
     return config;
 };
